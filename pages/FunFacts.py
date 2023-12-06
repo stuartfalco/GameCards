39264c3fe02ec46dcb0da2a5fcbc2647ad15cec0
stuartfalco <64333632+stuartfalco@users.noreply.github.com>
2023-12-05 18:12:40 -0800

    Update FunFacts.py

--- pages/FunFacts.py
+++ pages/FunFacts.py
@@ -2,38 +2,25 @@ import streamlit as st
 import random
 import os
 
-st.markdown("# Fun Facts 🎉")
-st.sidebar.markdown("# Fun Facts 🎉")
+from helper_fcn import load_text_file, shuffle_cards,draw_card , load_value_from_json
+###
 
-if st.checkbox("Read the Instructions"):
-    st.write("1. Reveal a new question & everyone secretly writes their answer on their arrow face down")
-    st.write("2. Take turns placing arrow in relation to others from LOW to HIGH")
-    st.write("3. After everyone has placed their answer, the starting player has the opportunity to move their own arrow — without touching anyone else's!")
-    st.write("4. Then you reveal everyone's numbers, and removes each arrow that is out of order.")
-    st.write("5. Each remaining arrow represents a correct answer and provides one point for the entire team. ")
-    st.write("6. After eight rounds, record the team's score in the Record of Legends.")
+game_name = 'FunFacts'
+
+st.markdown(f"# :green[{game_name}] ")
+
+file_path = os.path.join(os.getcwd(),'instruction.json')
+instructions = load_value_from_json(file_path, game_name)
+ 
+with st.expander('Click to toggle => Rules'):
+    for i, item in enumerate(instructions):
+        st.markdown(f"{i+1}. {item}")
     
 st.divider()
-def load_text_file(file_path):
-    try:
-        with open(file_path, 'r') as file:
-            lines = file.readlines()
-            lines = [line.strip() for line in lines]
-            return lines
-    except FileNotFoundError:
-        print(f"File '{file_path}' not found.")
-        return []
-    
-
-#cards = ['Ace', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King']
-file_path = os.path.join(os.getcwd(),'FunFacts.txt')
+ 
+file_name = game_name + ".txt"
+file_path = os.path.join(os.getcwd(),file_name)
 cards = load_text_file(file_path)
-
-def shuffle_cards(cards):
-    
-    shuffled_list = cards.copy()
-    random.shuffle(shuffled_list)
-    return shuffled_list
 
 deck = shuffle_cards(cards)
 
@@ -51,27 +38,12 @@ if 'count' not in st.session_state:
     st.session_state.count = 0 
 if 'rounds' not in st.session_state:
     st.session_state.rounds = 0 
-    
-def draw_card():
-    
-    new_card = st.session_state.deck1[st.session_state.count]
-    
-    if st.session_state.count < len(st.session_state.deck1)-1:
-        st.session_state.count += 1
-    else:
-        st.session_state.count = 0
         
-    
-      
-    return new_card 
-
-    
 sub1 = st.button("Next Card", key='rand1')
 if sub1:
     st.session_state.rounds += 1
     new_card = draw_card()
-    #st.write('OG Deck:', cards)
     st.markdown(f':red[rounds:] {st.session_state.rounds}')
     st.markdown('#QUESTION:')
-    st.markdown(f"# {new_card}")
+    st.markdown(f"# {new_card.upper()}")
     
